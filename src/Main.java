9b953379945d876130365215e321197003a115d1
Edwin <evalleb@miumg.edu.gt>
2022-07-22 23:40:53 -0600

    Inicio proyecto

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre:");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Bienvenido a Progra 2 2022 " + nombre);
+
+    }
+}
